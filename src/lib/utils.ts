904f27cfd82fd136f39d313a5a267ed60a25a34e
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2021-11-07 19:57:22 +0900

    cleanup

--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-export const mapObject = <T extends object, K extends keyof T, R>(
-  obj: T,
-  fn: (value: T[K], key: K) => R,
-) => {
-  return (Object.keys(obj) as K[]).map<R>(key => fn(obj[key], key))
-}
--- src/plugins/vue-modal.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueModal, { Options, createMediator } from '../lib'
-
-const mediator = createMediator()
-
-Vue.use<Options>(VueModal, { mediator })
-
-export default mediator
