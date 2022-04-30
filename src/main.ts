c4c386b521e3327af8e4ab803446f085bf0123d9
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2022-05-01 00:35:22 +0900

    use dynamic import with defineAsyncComponent

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 import App from "./App";
-import HelloModal from "./components/HelloModal";
 import { createModalManager } from "./plugins/modal";
 
 const modalManager = createModalManager();
-modalManager.addComponent("hello", HelloModal);
 
 const app = createApp(App);
 app.use(modalManager);
